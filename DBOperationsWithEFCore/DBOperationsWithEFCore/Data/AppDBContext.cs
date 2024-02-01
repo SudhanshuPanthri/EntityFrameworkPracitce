38da44f640aa7ecb841d49c77c406402502bce8c
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-02-01 22:01:59 +0530

    First Commit

---
+++ DBOperationsWithEFCore/DBOperationsWithEFCore/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DBOperationsWithEFCore.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
